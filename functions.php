b05312e789f3fca89efd5617f9f887bc742e51c9
konverts <42354535+konverts@users.noreply.github.com>
2018-08-16 19:34:01 +0300

    homework

---
+++ functions.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: DanStart
+ * Date: 16.08.2018
+ * Time: 19:05
+ */
+
+function show_something()
+{
+    return 'SOMETHING';
+}
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+require_once 'functions.php';
+
+const TEST_CONSTANTA = 'asd';
+$user_name = "Igor";
+
+if (1==1) {
+    echo 'hi';
+}
